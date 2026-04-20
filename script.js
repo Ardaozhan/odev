9e3956297d6d33078041dc24c3daa31fcb6bcc5c
Ardaozhan <ardaozhan39@gmail.com>
2026-04-20 15:36:35 +0300

    Add turk_kahvesi.jpg image file to the repository

---
+++ script.js
@@ -0,0 +1,7 @@
+// Sayfalar açılınca konsola yazar
+console.log("Sitemiz sorunsuz çalışıyor.");
+
+function mesajGonder() {
+    // İletişim sayfasındaki butona basınca bu çıkacak
+    alert("Sistemde bir hata oldu galiba. Mesajınız gitmedi, en iyisi siz bizi telefondan arayın.");
+}
